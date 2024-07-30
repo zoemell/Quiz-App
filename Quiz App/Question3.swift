cef7fa988fe7ea878086d4b05a26477677929721
Zoe Meller <zoeyaelmeller@gmail.com>
2024-07-30 10:50:24 -0400

    updated quiz app with finished question 1,2,3 and end page

---
+++ Quiz App/Question3.swift
@@ -0,0 +1,18 @@
+//
+//  Question3.swift
+//  Quiz App
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct Question3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Question3()
+}
---
+++ Quiz App/QuizEnd.swift
@@ -0,0 +1,18 @@
+//
+//  QuizEnd.swift
+//  Quiz App
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct QuizEnd: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizEnd()
+}
